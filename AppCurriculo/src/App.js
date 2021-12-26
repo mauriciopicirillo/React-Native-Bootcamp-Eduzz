ff57aba3d1ac430b4ee0493e22e1094cd9d68de5
Mauricio Aparecido Picirillo <79844553+mauriciopicirillo@users.noreply.github.com>
2021-12-26 17:29:11 -0300

    Primeiros passos concluidos
    Nesse ponto aprendi a colocar icones, ajustar com o flexbox e alinhar os objetos

--- AppCurriculo/src/App.js
+++ AppCurriculo/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import {StyleSheet, View, Image, Text} from 'react-native';
+import {StyleSheet, View, Image, Text, SafeAreaView, TouchableOpacity, Alert} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 
 import foto from './assets/foto.jpeg';
 
 const App = () => {
+
+    function handleRedeSocial(rede_social){
+      switch(rede_social){
+        case 'linkedin':
+          Alert.alert('Meu Linkedin:','https://www.linkedin.com/in/mauricio-aparecido-picirillo-34907246/')
+        break
+        case 'facebook':
+        Alert.alert('Meu Facebook:','https://www.facebook.com/mauricio.aparecidopicirillo')
+        break
+        case 'github':
+          Alert.alert('Meu GitHub:','https://github.com/mauriciopicirillo')
+        break  
+      }
+    }
+
   return (
     <>
       <View style={style.page}>
@@ -12,11 +27,45 @@ const App = () => {
           <Image source={foto} style={style.foto} />
           <Text style={style.nome}>MAURICIO AP. PICIRILLO</Text>
           <Text style={style.funcao}>Desenvolvedor Mobile</Text>
-          <View>
-            <Icon name="github" size={30} />
-            <Icon name="github" size={30} />
-            <Icon name="github" size={30} />
+          <View style={style.redes_socias}>
+           
+            <TouchableOpacity onPress={() => handleRedeSocial('github')}> 
+              <Icon name="github" size={30} /> 
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => handleRedeSocial('facebook')}> 
+              <Icon name="facebook" size={30} /> 
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => handleRedeSocial('linkedin')}> 
+              <Icon name="linkedin" size={30} /> 
+            </TouchableOpacity>
+                       
           </View>
+        </View>
+
+        <View style={style.card_container}>
+        <View style={style.card}>
+          <View style={style.card_header}>
+            <Text>Experiencia Profissional</Text>
+          </View>
+          <View style={style.card_content}>
+          <Text style={style.card_content_text}>Ditus Company</Text>
+          <Text style={style.card_content_text}>Rafard World Club</Text>
+          <Text style={style.card_content_text}>Hersheys</Text>
+          </View>
+        </View>
+        </View>
+
+        <View style={style.card_container}>
+        <View style={style.card}>
+          <View style={style.card_header}>
+            <Text>Formação Academica</Text>
+          </View>
+          <View style={style.card_content}>
+          <Text style={style.card_content_text}>Cruzeiro do Sul</Text>
+          <Text style={style.card_content_text}>Digital Innovation One</Text>
+          <Text style={style.card_content_text}>Microcamp</Text>
+          </View>
+        </View>
         </View>
       </View>
     </>
@@ -30,9 +79,9 @@ const style = StyleSheet.create({
   },
 
   container_cabecalho: {
-    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 50,
   },
 
   foto: {
@@ -51,6 +100,38 @@ const style = StyleSheet.create({
     color: '#939393',
     marginBottom: 10,
   },
+
+  redes_socias: {
+    fontSize: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '50%',
+    marginTop: 20,
+  },
+
+  card_container: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+
+  card: {
+    width: '60%',
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#939393',
+    padding: 10,
+    backgroundColor: '#FFF',
+  },
+  
+  card_content: {
+    marginTop: 10,
+  },
+  card_content_text: {
+    color: '#939393',
+    marginBottom: 10,
+  }
 });
 
 export default App;
