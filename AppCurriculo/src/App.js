fc414dce62aa57343b5fcbe9ad002579bd9114b3
Mauricio Aparecido Picirillo <79844553+mauriciopicirillo@users.noreply.github.com>
2021-12-26 13:09:26 -0300

    Erro no App
    Procurando erro no código do aplicativo que parou de funcionar

--- AppCurriculo/src/App.js
+++ AppCurriculo/src/App.js
@@ -1,37 +1,56 @@
 import React from 'react';
-import {StyleSheet, View, Image} from 'react-native';
+import {StyleSheet, View, Image, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import foto from './assets/foto.jpeg';
 
 const App = () => {
-    return (
+  return (
     <>
       <View style={style.page}>
         <View style={style.container_cabecalho}>
-          <Image source={foto} style={style.foto}/>
+          <Image source={foto} style={style.foto} />
+          <Text style={style.nome}>MAURICIO AP. PICIRILLO</Text>
+          <Text style={style.funcao}>Desenvolvedor Mobile</Text>
+          <View>
+            <Icon name="github" size={30} />
+            <Icon name="github" size={30} />
+            <Icon name="github" size={30} />
+          </View>
         </View>
-
       </View>
-    </> 
+    </>
   );
 };
 
 const style = StyleSheet.create({
-  page:{
+  page: {
     backgroundColor: '#E7E7E7',
     flex: 1,
   },
+
   container_cabecalho: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
+
   foto: {
     width: 250,
     height: 250,
     borderRadius: 125,
-  }
-})
+  },
 
+  nome: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+
+  funcao: {
+    color: '#939393',
+    marginBottom: 10,
+  },
+});
 
 export default App;
